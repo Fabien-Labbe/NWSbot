3be8408f3cccf555e499eb1937996501d957fb67
Fabien <ahku.fl@gmail.com>
2020-06-03 11:02:08 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+const Discord = require('discord.js')
+const client = new Discord.Client()
+
+client.on('message', msg =>{
+    if (msg.content === 'ping'){
+        msg.reply('Pong')
+    }
+});
+
+client.login(process.env.discord_token);
