5cd0a550d340e589e32f58e580fdbec8d33092eb
Fabien <ahku.fl@gmail.com>
2020-06-03 12:20:11 +0200

    chat

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const Discord = require('discord.js');
 const Request = require('./request')
 
 const client = new Discord.Client()
-
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
 
 
 client.on('ready', ()=>{
@@ -12,14 +14,25 @@ client.on('message', msg =>{
     if (msg.content === 'ping'){
         msg.reply('Pong')
     }
+    if (msg.content === 'chat'){
+        let catArray = [
+            'https://media.giphy.com/media/RhrAmVUHxjTQvEPBWi/giphy.gif',
+            'https://gph.is/1fIfMKt',
+            'https://media.giphy.com/media/C9x8gX02SnMIoAClXa/giphy.gif',
+            'https://media.giphy.com/media/tBxyh2hbwMiqc/giphy.gif',
+            'https://media.giphy.com/media/quO0X65yj6gw0/giphy.gif'
+
+        ]
+        msg.reply(catArray[getRandomInt(5)])
+    }
     let a = [
         [
         msg.content, 
         msg.author.username,
-        new Date().now()
+        new Date()
         ]
     ]
     Request.Insert(a);
 });
 
-client.login(process.env.discord_token);
+client.login('NzE3NjQ3NzE5MzA0Mzk2ODIx.Xtdz9A.wQqCIcmwDI3f0wgVj56pNcOF5Cs');
