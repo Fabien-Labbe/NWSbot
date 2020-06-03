0fbc395d4aea8fa61cb3310565464d9852a36504
Fabien <ahku.fl@gmail.com>
2020-06-03 11:10:58 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
 
+client.on('ready', ()=>{
+    console.log('Bot ready')
+})
 client.on('message', msg =>{
     if (msg.content === 'ping'){
         msg.reply('Pong')
