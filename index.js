ebc784c9eac9d956afa3c7872b63fd36fa6cc7c0
Fabien <ahku.fl@gmail.com>
2020-06-03 11:46:07 +0200

    sql

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
-const Discord = require('discord.js')
+const Discord = require('discord.js');
+const Request = require('./request')
+
 const client = new Discord.Client()
 
+
+
 client.on('ready', ()=>{
-    console.log('Bot ready')
+    console.log('Logged in as ${client.user.tag} !')
 })
 client.on('message', msg =>{
     if (msg.content === 'ping'){
         msg.reply('Pong')
     }
+    let a = [
+        [
+        msg.content, 
+        msg.author.username,
+        new Date().now()
+        ]
+    ]
+    Request.Insert(a);
 });
 
 client.login(process.env.discord_token);
---
+++ request.js
@@ -0,0 +1,19 @@
+const mysql = require(mysql);
+
+const requeteInsert = "INSERT INTO messages (Message, User, Heure) VALUES ?"; 
+
+let connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "bot-discord"
+  });
+  
+  connection.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected !");
+  });
+
+  exports.Insert = (values)=>{
+      connection.query(requeteInsert, [values])
+  }
